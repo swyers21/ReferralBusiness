6b1ead6535f777de777b1e2193c9735010afa6b6
creativeautomaton <creativeautomaton@gmail.com>
2019-07-17 15:53:30 -0500

    tokenize and input to just working

--- js/init.js
+++ js/init.js
@@ -1,11 +1,42 @@
+
 (function($){
   $(function(){
 
     $('.button-collapse').sideNav();
 
-    export function formsubmit(){
-      console.log('called formsubmit');
+    function getFormData($form){
+        var unindexed_array = $form.serializeArray();
+        var indexed_array = {};
+
+        $.map(unindexed_array, function(n, i){
+            indexed_array[n['name']] = n['value'];
+        });
+
+        return indexed_array;
     }
+    function storeData(data){
+      console.log(data);
+    }
+
+    function formSubmit(){
+     console.log('called form Submit');
+      var $form = $("#referral-form");
+      var data = getFormData($form);
+      // getTags();
+      // console.log( tags.getTags() );
+      storeData(data);
+   }
+
+
+    $( document ).ready(function() {
+        console.log( "ready!" );
+
+       $('.form-submit').on( 'click', function(){
+            formSubmit()
+       });
+
+    });
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
